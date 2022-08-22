bd9b4875ea8be5b0a5d5ae5fe253a0f6f7b11a0f
Hypermona <mohans8050@gmail.com>
2022-08-22 15:43:32 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="head">
+      <div className="logo">SCG</div>
+      <div className="=links">Links</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,7 @@
+import "./main.css";
+
+const Main = () => {
+  return <h1 className="main">Main</h1>;
+};
+
+export default Main;
